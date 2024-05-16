70639e5a8e87d308385232b82d3c09af36f09d57
Beaskyy <afeez.akintobi@yahoo.com>
2024-05-17 00:19:07 +0100

    authentication

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <main className="flex justify-center items-center min-h-screen">
-      <Button variant="default">Button</Button>
-    </main>
-  );
-}
