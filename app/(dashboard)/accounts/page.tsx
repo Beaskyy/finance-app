48c66106b6798c05d5d3e52dbf8f7195d6559b99
Beaskyy <afeez.akintobi@yahoo.com>
2024-06-11 22:37:43 +0100

    add accounts table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { Plus } from "lucide-react";
+import { Payment, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+];
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
@@ -16,6 +27,9 @@ const AccountsPage = () => {
             <Plus className="size-4 mr-2" /> Add new
           </Button>
         </CardHeader>
+        <CardContent>
+        <DataTable columns={columns} data={data} />
+        </CardContent>
       </Card>
     </div>
   );
